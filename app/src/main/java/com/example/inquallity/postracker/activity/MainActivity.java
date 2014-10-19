f0699fbcc8ac8f778a67297e879a2c0f45a49173
inquallity <inquallity@gmail.com>
2014-10-19 22:59:51 +0400

    step 3.1

--- app/src/main/java/com/example/inquallity/postracker/activity/MainActivity.java
+++ app/src/main/java/com/example/inquallity/postracker/activity/MainActivity.java
@@ -1,35 +1,15 @@
 package com.example.inquallity.postracker.activity;
 
 import android.app.Activity;
-import android.content.ContentValues;
 import android.content.Intent;
-import android.location.Address;
-import android.location.Geocoder;
-import android.location.Location;
 import android.os.Bundle;
-import android.util.Log;
 
 import com.example.inquallity.postracker.R;
 import com.example.inquallity.postracker.fragment.TrackCollect;
 import com.example.inquallity.postracker.service.TrackService;
-import com.example.inquallity.postracker.sqlite.TrackSql;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
-
-import java.io.IOException;
-import java.util.List;
-import java.util.Locale;
 
 
-public class MainActivity extends Activity implements
-        LocationListener,GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener {
-
-    private GoogleApiClient mGoogleApi;
-    private LocationRequest mRequest;
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,75 +21,8 @@ public class MainActivity extends Activity implements
                     .add(R.id.frame, new TrackCollect())
                     .commit();
         }
-
         startService(new Intent(getApplicationContext(), TrackService.class));
-
-        if (mGoogleApi == null) {
-            mGoogleApi = new GoogleApiClient.Builder(getApplicationContext())
-                    .addApi(LocationServices.API)
-                    .addConnectionCallbacks(this)
-                    .addOnConnectionFailedListener(this)
-                    .build();
-        }
-        mRequest = LocationRequest.create();
-        mRequest.setInterval(5000);
-        mRequest.setFastestInterval(1000);
-        mGoogleApi.connect();
     }
 
-    @Override
-    public void onConnected(Bundle bundle) {
-        Log.i("TAG", "GoogleApi connected");
-        final Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApi);
-        if (location != null) {
-            onLocationChanged(location);
-        }
-        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApi, mRequest, this);
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-        retryConnecting();
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-        ContentValues cv = new ContentValues();
-        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
-        List<Address> addresses = null;
-        String textAddr = null;
-
-        try {
-            addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
-        } catch (IOException e) {
-            Log.d("TAG", "EXCEPTION ");
-            e.printStackTrace();
-        }
-        if (addresses == null) {
-            Log.d("TAG", "Addresses is null");
-
-        } else if (addresses.size() == 0) {
-            Log.d("TAG", "List of the addresses is empty");
-        } else {
-            Address address = addresses.get(0);
-            textAddr = address.getLocality() + ", " + address.getCountryName();
-        }
-
-        cv.put("position", textAddr);
-        cv.put("latitude", location.getLatitude());
-        cv.put("longitude", location.getLongitude());
-        getContentResolver().insert(TrackSql.URI, cv);
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult result) {
-        retryConnecting();
-    }
-
-    private void retryConnecting() {
-        if (!mGoogleApi.isConnecting()) {
-            mGoogleApi.connect();
-        }
-    }
-}
+ }
 
--- app/src/main/java/com/example/inquallity/postracker/service/TrackService.java
+++ app/src/main/java/com/example/inquallity/postracker/service/TrackService.java
@@ -1,14 +1,35 @@
 package com.example.inquallity.postracker.service;
 
 import android.app.Service;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
 
-public class TrackService extends Service{
+import com.example.inquallity.postracker.sqlite.TrackSql;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class TrackService extends Service implements
+        LocationListener,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
 
     private static final String TAG = "TAG_OF_SERVICE";
+    private GoogleApiClient mGoogleApi;
+    private LocationRequest mRequest;
 
     @Override
     public void onCreate() {
@@ -16,6 +37,17 @@ public class TrackService extends Service{
         Log.d(TAG, "Service has been started");
         Toast.makeText(getApplicationContext(), "Service has been started", Toast.LENGTH_LONG).show();
 
+        if (mGoogleApi == null) {
+            mGoogleApi = new GoogleApiClient.Builder(getApplicationContext())
+                    .addApi(LocationServices.API)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .build();
+        }
+        mRequest = LocationRequest.create();
+        mRequest.setInterval(5000);
+        mRequest.setFastestInterval(1000);
+        mGoogleApi.connect();
     }
 
     //Обработка некорректного завершения сервиса
@@ -48,4 +80,58 @@ public class TrackService extends Service{
     }
 
 
+    @Override
+    public void onLocationChanged(Location location) {
+        ContentValues cv = new ContentValues();
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        List<Address> addresses = null;
+        String textAddr = null;
+
+        try {
+            addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+        } catch (IOException e) {
+            Log.d("TAG", "EXCEPTION ");
+            e.printStackTrace();
+        }
+        if (addresses == null) {
+            Log.d("TAG", "Addresses is null");
+
+        } else if (addresses.size() == 0) {
+            Log.d("TAG", "List of the addresses is empty");
+        } else {
+            Address address = addresses.get(0);
+            textAddr = address.getLocality() + ", " + address.getCountryName();
+        }
+
+        cv.put("position", textAddr);
+        cv.put("latitude", location.getLatitude());
+        cv.put("longitude", location.getLongitude());
+        getContentResolver().insert(TrackSql.URI, cv);
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        retryConnecting();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.i("TAG", "GoogleApi connected");
+        final Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApi);
+        if (location != null) {
+            onLocationChanged(location);
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApi, mRequest, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        retryConnecting();
+    }
+
+    private void retryConnecting() {
+        if (!mGoogleApi.isConnecting()) {
+            mGoogleApi.connect();
+        }
+    }
 }
